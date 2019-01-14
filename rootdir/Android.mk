c039f0262ead9b07fe0f24faed62f3910024e21b
celtare21 <celtare21@gmail.com>
2019-01-14 13:42:19 +0000

    sdm845-common: rootdir: Tune ramdisk
    Change-Id: I90b0ea89e1ea12f2519dfb60f2e9ac918e0a8927
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,6 +24,14 @@ LOCAL_SRC_FILES    := bin/init.jamesdsp.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.performance.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.performance.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/init
+include $(BUILD_PREBUILT)
+
 # Offmode charging
 include $(CLEAR_VARS)
 LOCAL_MODULE          := chargeonlymode
