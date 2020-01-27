88f3d5f3397e4f1b04eb5a9c00398beaa4b7b072
doc HD <doc.divxm@gmail.com>
2020-01-27 20:40:35 +0300

    sdm845-common: use aicp.AppHelper
    Change-Id: I11297ea5b9075b24e845e2acb983efb6903223ba

--- ScreenOffGestures/src/com/aicp/device/util/ShortcutPickerHelper.java
+++ ScreenOffGestures/src/com/aicp/device/util/ShortcutPickerHelper.java
@@ -30,7 +30,7 @@ import android.util.Log;
 
 import com.aicp.device.R;
 
-import com.android.internal.util.gzosp.AppHelper;
+import com.android.internal.util.aicp.AppHelper;
 
 import java.util.ArrayList;
 
